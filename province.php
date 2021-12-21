92105ee12a9a7365e89e048f6697f9561f9fec51
Paiwaii <72967123+Paiwaii@users.noreply.github.com>
2021-12-21 22:51:21 +0700

    Update province.php

--- province.php
+++ province.php
@@ -9,10 +9,10 @@
 echo '<body style="background-color:...">';
 echo "<center><table border='1'>";
 echo "<tr><td style= 'background-color:9999CC'>ลำดับ</td>
-      <td style= 'background-color:E9967A'>จังหวัด</td>
+      <td style= 'background-color:9999CC'>จังหวัด</td>
       <td style= 'background-color:ADD8E6'>จำนวนผู้ป่วยใหม่</td>
       <td style= 'background-color:99FFCC'>จำนวนผู้ป่วยสะสม</td>
-      <td style= 'background-color:FFFF66'>จำนวนผู้ป่วยใหม่(รวมต่างประเทศ)</td>
+      <td style= 'background-color:E9967A'>จำนวนผู้ป่วยใหม่(รวมต่างประเทศ)</td>
       <td style= 'background-color:FFCC33'>จำนวนผู้ป่วยสะสม(รวมต่างประเทศ)</td>
       <td style= 'background-color:'>จำนวนผู้เสียชีวิต</td>
       <td style= 'background-color:...'>จำนวนผู้เสียชีวิตสะสม</td>
@@ -23,7 +23,7 @@ foreach($data as $key=>$val){
   echo "<td style= 'background-color:E6E6FA'>";
   echo ($key+1);
   echo "</td>";
-  echo "<td style= 'background-color:FFE4E1'>";
+  echo "<td style= 'background-color:E6E6FA'>";
   echo $val->province;
   echo "</td>";
   echo "<td style= 'background-color:F0F8FF'>";
