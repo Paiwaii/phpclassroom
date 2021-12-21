4ab4c095cabe472d14da98106450c4f43b9e4dc9
Paiwaii <72967123+Paiwaii@users.noreply.github.com>
2021-12-21 22:52:45 +0700

    Update province.php

--- province.php
+++ province.php
@@ -11,7 +11,7 @@ echo "<center><table border='1'>";
 echo "<tr><td style= 'background-color:9999CC'>ลำดับ</td>
       <td style= 'background-color:9999CC'>จังหวัด</td>
       <td style= 'background-color:ADD8E6'>จำนวนผู้ป่วยใหม่</td>
-      <td style= 'background-color:99FFCC'>จำนวนผู้ป่วยสะสม</td>
+      <td style= 'background-color:ADD8E6'>จำนวนผู้ป่วยสะสม</td>
       <td style= 'background-color:E9967A'>จำนวนผู้ป่วยใหม่(รวมต่างประเทศ)</td>
       <td style= 'background-color:FFCC33'>จำนวนผู้ป่วยสะสม(รวมต่างประเทศ)</td>
       <td style= 'background-color:'>จำนวนผู้เสียชีวิต</td>
@@ -29,7 +29,7 @@ foreach($data as $key=>$val){
   echo "<td style= 'background-color:F0F8FF'>";
   echo $val->new_case;
   echo "</td>";
-  echo "<td style= 'background-color:F0FFF0'>";
+  echo "<td style= 'background-color:F0F8FF'>";
   echo $val->total_case;
   echo "</td>";
   echo "<td style= 'background-color:FFF8DC'>";
