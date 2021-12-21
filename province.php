30010b50ead70ebfd6638074dddc4c29c69d3d15
Paiwaii <72967123+Paiwaii@users.noreply.github.com>
2021-12-21 14:28:58 +0700

    Update province.php

--- province.php
+++ province.php
@@ -11,10 +11,10 @@ echo "<center><table border='1'>";
 echo "<tr><td style= 'background-color:9999CC'>ลำดับ</td>
       <td style= 'background-color:FFC0CB'>จังหวัด</td>
       <td style= 'background-color:ADD8E6'>จำนวนผู้ป่วยใหม่</td>
-      <td style= 'background-color:F0FFF0'>จำนวนผู้ป่วยสะสม</td>
-      <td style= 'background-color:66CC66'>จำนวนผู้ป่วยใหม่(รวมต่างประเทศ)</td>
-      <td style= 'background-color:FFFF33'>จำนวนผู้ป่วยสะสม(รวมต่างประเทศ)</td>
-      <td style= 'background-color:FFCC33'>จำนวนผู้เสียชีวิต</td>
+      <td style= 'background-color:99FFCC'>จำนวนผู้ป่วยสะสม</td>
+      <td style= 'background-color:FFD700'>จำนวนผู้ป่วยใหม่(รวมต่างประเทศ)</td>
+      <td style= 'background-color:FFCC33'>จำนวนผู้ป่วยสะสม(รวมต่างประเทศ)</td>
+      <td style= 'background-color:'>จำนวนผู้เสียชีวิต</td>
       <td style= 'background-color:...'>จำนวนผู้เสียชีวิตสะสม</td>
       </tr>";
   
