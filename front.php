774cf03f46d79638844848467280bf9a572fadbe
Paiwaii <72967123+Paiwaii@users.noreply.github.com>
2022-01-04 09:17:57 +0700

    Update front.php

--- front.php
+++ front.php
@@ -4,38 +4,37 @@
     <!-- Required meta tags -->
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1">
-
     <!-- Bootstrap CSS -->
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3" crossorigin="anonymous">
-
     <title>Hello, world!</title>
     
     <style>
       #col1{
-        background: red;
+        background-color: red;
       }
       
       #col2{
-        background: green;
+        background-color: green;
       }
       
-      #col1{
-        background: blue;
+      #col3{
+        background-color: blue;
       }
-     </style>
+    </style>
+    
   </head>
   <body>
-     <div class="container">
-       <div class="row">
-         <div id="col1" class="col-4">
-           Column
-       </div>
-         <div id="col2" class="col-4">
-           Column
-       </div>
-         <div id="col3" class="col">
-           Column
-       </div>
+    <div class="container">
+      <div class="row">
+        <div id="col1" class="col-4">
+          Column
+        </div>
+        <div id="col2"  class="col-4">
+          Column
+        </div>
+        <div id="col3"  class="col-4">
+          Column
+        </div>
       </div>
     </div>
   </body>
